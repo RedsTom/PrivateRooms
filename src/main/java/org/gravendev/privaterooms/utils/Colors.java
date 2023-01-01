35613dba51a4171220a11a04bd80aa8cdb0a5ce8
Tom BUTIN <tom.frefre.13@gmail.com>
2023-01-01 18:38:47 +0100

    Added a utility class for colors

---
+++ src/main/java/org/gravendev/privaterooms/utils/Colors.java
@@ -0,0 +1,29 @@
+/*
+ * PrivateRooms is a bot managing vocal channels in a server
+ * Copyright (C) 2022 RedsTom
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.gravendev.privaterooms.utils;
+
+import java.awt.*;
+
+public interface Colors {
+    Color RED = new Color(0xF03A47);
+    Color ORANGE = new Color(0xFFA400);
+    Color BLUE = new Color(0x276FBF);
+    Color GREEN = new Color(0x03CEA4);
+    Color WHITE = new Color(0xF6F4F3);
+}
